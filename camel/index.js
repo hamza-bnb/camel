7d03197a9560279dd8f7307605b568c958fbe9d2
hamza-bnb <141778646+hamza-bnb@users.noreply.github.com>
2023-08-08 23:39:04 +0300

    Add files via upload

---
+++ camel/index.js
@@ -0,0 +1,524 @@
+let myid = document.getElementById("front")
+
+
+myid.onclick = function() {
+    document.getElementById("front").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid1 = document.getElementById("back")
+
+
+myid1.onclick = function() {
+    document.getElementById("front").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+let myid2 = document.getElementById("front-1")
+
+
+myid2.onclick = function() {
+    document.getElementById("front-1").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-1").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid3 = document.getElementById("back-1")
+
+
+myid3.onclick = function() {
+    document.getElementById("front-1").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-1").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+
+let myid4 = document.getElementById("front-2")
+
+
+myid4.onclick = function() {
+    document.getElementById("front-2").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-2").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid5 = document.getElementById("back-2")
+
+
+myid5.onclick = function() {
+    document.getElementById("front-2").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-2").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+let myid6 = document.getElementById("front-3")
+
+
+myid6.onclick = function() {
+    document.getElementById("front-3").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-3").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid7 = document.getElementById("back-3")
+
+
+myid7.onclick = function() {
+    document.getElementById("front-3").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-3").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+let myid8 = document.getElementById("front-4")
+
+
+myid8.onclick = function() {
+    document.getElementById("front-4").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-4").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid9 = document.getElementById("back-4")
+
+
+myid9.onclick = function() {
+    document.getElementById("front-4").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-4").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+let myid10 = document.getElementById("front-5")
+
+
+myid10.onclick = function() {
+    document.getElementById("front-5").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-5").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid11 = document.getElementById("back-5")
+
+
+myid11.onclick = function() {
+    document.getElementById("front-5").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-5").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+
+let myid12 = document.getElementById("front-6")
+
+
+myid12.onclick = function() {
+    document.getElementById("front-6").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-6").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid13 = document.getElementById("back-6")
+
+
+myid13.onclick = function() {
+    document.getElementById("front-6").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-6").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+let myid14 = document.getElementById("front-7")
+
+
+myid14.onclick = function() {
+    document.getElementById("front-7").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-7").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid15 = document.getElementById("back-7")
+
+
+myid15.onclick = function() {
+    document.getElementById("front-7").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-7").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+let myid16 = document.getElementById("front-8")
+
+
+myid16.onclick = function() {
+    document.getElementById("front-8").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-8").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid17 = document.getElementById("back-8")
+
+
+myid17.onclick = function() {
+    document.getElementById("front-8").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-8").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+
+let myid18 = document.getElementById("front-9")
+
+
+myid18.onclick = function() {
+    document.getElementById("front-9").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-9").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid19 = document.getElementById("back-9")
+
+
+myid19.onclick = function() {
+    document.getElementById("front-9").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-9").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+// let myid20 = document.getElementById("front-10")
+
+
+// myid20.onclick = function() {
+//     document.getElementById("front-10").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+//     document.getElementById("back-10").style.transform = "translateX(-17%) rotateY(0deg)";
+// }
+
+
+// let myid21 = document.getElementById("back-10")
+
+
+// myid21.onclick = function() {
+//     document.getElementById("front-10").style.transform = "translateX(0%) rotateY(0deg)";
+
+//     document.getElementById("back-10").style.transform = "translateX(-71%) rotateY(-90deg)";
+// }
+
+
+
+
+
+
+
+
+
+// let myid20 = document.getElementById("front-10")
+
+
+// myid20.onclick = function() {
+//     document.getElementById("front-10").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+//     document.getElementById("back-10").style.transform = "translateX(-17%) rotateY(0deg)";
+// }
+
+
+// let myid21 = document.getElementById("back-10")
+
+
+// myid21.onclick = function() {
+//     document.getElementById("front-10").style.transform = "translateX(0%) rotateY(0deg)";
+
+//     document.getElementById("back-10").style.transform = "translateX(-71%) rotateY(-90deg)";
+// }
+
+
+
+
+
+
+
+
+
+
+
+let myid20 = document.getElementById("front-10")
+
+
+myid20.onclick = function() {
+    document.getElementById("front-10").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-10").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid21 = document.getElementById("back-10")
+
+
+myid21.onclick = function() {
+    document.getElementById("front-10").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-10").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+let myid22 = document.getElementById("front-11")
+
+
+myid22.onclick = function() {
+    document.getElementById("front-11").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-11").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid23 = document.getElementById("back-11")
+
+
+myid23.onclick = function() {
+    document.getElementById("front-11").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-11").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+
+let myid24 = document.getElementById("front-12")
+
+
+myid24.onclick = function() {
+    document.getElementById("front-12").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-12").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid25 = document.getElementById("back-12")
+
+
+myid25.onclick = function() {
+    document.getElementById("front-12").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-12").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+
+let myid26 = document.getElementById("front-13")
+
+
+myid26.onclick = function() {
+    document.getElementById("front-13").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-13").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid27 = document.getElementById("back-13")
+
+
+myid27.onclick = function() {
+    document.getElementById("front-13").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-13").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+
+let myid28 = document.getElementById("front-14")
+
+
+myid28.onclick = function() {
+    document.getElementById("front-14").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-14").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid29 = document.getElementById("back-14")
+
+
+myid29.onclick = function() {
+    document.getElementById("front-14").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-14").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+let myid30 = document.getElementById("front-15")
+
+
+myid30.onclick = function() {
+    document.getElementById("front-15").style.transform = "translateX(-71%) rotateY(-90deg)";
+
+    document.getElementById("back-15").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid31 = document.getElementById("back-15")
+
+
+myid31.onclick = function() {
+    document.getElementById("front-15").style.transform = "translateX(0%) rotateY(0deg)";
+
+    document.getElementById("back-15").style.transform = "translateX(-71%) rotateY(-90deg)";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let myid300 = document.getElementById("frontt")
+
+
+myid300.onclick = function() {
+    document.getElementById("frontt").style.transform = "translateX(-71%) rotateY(0deg)";
+
+    document.getElementById("backk").style.transform = "translateX(-17%) rotateY(0deg)";
+}
+
+
+let myid310 = document.getElementById("backk")
+
+
+myid310.onclick = function() {
+    document.getElementById("frontt").style.transform = "translateX(71%) rotateY(-90deg)";
+
+    document.getElementById("backk").style.transform = "translateX(17%) rotateY(-90deg)";
+}
